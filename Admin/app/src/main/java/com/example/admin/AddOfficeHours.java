f591bf0a677599d82675ac43957516ae315426ea
ayesha159 <38466582+ayesha159@users.noreply.github.com>
2019-11-26 20:42:16 +0200

    abc

---
+++ Admin/app/src/main/java/com/example/admin/AddOfficeHours.java
@@ -0,0 +1,14 @@
+package com.example.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddOfficeHours extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_office_hours);
+    }
+}
---
+++ Admin/app/src/main/java/com/example/admin/URls.java
@@ -0,0 +1,9 @@
+package com.example.admin;
+
+public class URls {
+    private static final String ROOT_URL = "http://10.5.5.56/Admin/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+}
