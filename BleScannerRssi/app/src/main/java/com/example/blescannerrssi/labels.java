a9abec12ec877de9d85b3591d06082fb046b5cdf
Tooba3Naseer <42033832+Tooba3Naseer@users.noreply.github.com>
2019-11-26 20:14:52 +0500

    .

---
+++ BleScannerRssi/app/src/main/java/com/example/blescannerrssi/labels.java
@@ -0,0 +1,41 @@
+package com.example.blescannerrssi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class labels extends AppCompatActivity {
+
+    private Button btn_Start;
+    EditText editText, editText2, editText3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_labels);
+
+        btn_Start = (Button) findViewById(R.id.btnS);
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editText3 = (EditText) findViewById(R.id.editText3);
+
+
+        findViewById(R.id.btnS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(labels.this, MainActivity.class);
+
+                intent.putExtra("key", editText.getText().toString());
+                Toast.makeText(labels.this,editText.getText().toString() , Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+    }
+}
