23d1ace3dd2a4add63d7ec4fffe1c947faae1386
Tooba3Naseer <42033832+Tooba3Naseer@users.noreply.github.com>
2020-06-30 22:17:53 +0500

    .

--- SimplifiedCoding/app/src/main/java/com/example/simplifiedcoding/URls.java
+++
@@ -1,9 +0,0 @@
-package com.example.simplifiedcoding;
-
-public class URls {
-    private static final String ROOT_URL = "http://10.5.5.56/test/Api.php?apicall=";
-
-    public static final String URL_REGISTER = ROOT_URL + "signup";
-    public static final String URL_LOGIN= ROOT_URL + "login";
-
-}
